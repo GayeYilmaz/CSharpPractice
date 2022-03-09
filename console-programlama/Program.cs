9815b8827e598e22e201726058c97e31aafdb7fd
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-09 09:06:04 +0300

    Initial commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi giriniz :");
+            string surname = Console.ReadLine();
+             Console.WriteLine("Merhaba "+name+" "+surname);
+            Console.ReadLine();
+        }
+    }
+}
